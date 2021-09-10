333774fbfc7655540cd2bd5a2bae1e0453ac3867
Mirraim <mirraim@gmail.com>
2021-09-10 12:42:43 +0500

    1. PsqlStore. [#281226]

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE post
+(
+    id   SERIAL PRIMARY KEY,
+    name TEXT
+);
+
+CREATE TABLE candidate
+(
+    id   SERIAL PRIMARY KEY,
+    name TEXT
+);
---
+++ src/main/java/ru/job4j/dream/store/PsqlMain.java
@@ -0,0 +1,22 @@
+package ru.job4j.dream.store;
+
+import ru.job4j.dream.model.Candidate;
+import ru.job4j.dream.model.Post;
+
+public class PsqlMain {
+    public static void main(String[] args) {
+        Store store = PsqlStore.instOf();
+        store.save(new Post(0,"Java Job"));
+        store.save(new Post(0, "Middle Java Job"));
+        for (Post post : store.findAllPosts()) {
+            System.out.println(post.getId() + " " + post.getName());
+        }
+        for (Candidate candidate : store.findAllCandidates()) {
+            System.out.println(candidate.getId() + " " + candidate.getName());
+        }
+        System.out.println("-------------------------");
+        store.save(new Post(1, "Junior Java Job"));
+        Post post = store.findById(1);
+        System.out.println(post.getId() + " " + post.getName());
+    }
+}
