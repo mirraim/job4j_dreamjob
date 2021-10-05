8acd7a5cde9a1feccad51eeb09c538e32b4cbb55
Mirraim <mirraim@gmail.com>
2021-10-05 13:13:10 +0500

    4.1. JSON формат. [#238554]

---
+++ src/main/java/ru/job4j/dream/model/Email.java
@@ -0,0 +1,17 @@
+package ru.job4j.dream.model;
+
+public class Email {
+    private String name;
+
+    public Email(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
